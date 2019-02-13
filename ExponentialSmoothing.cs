24e780e5ce1d73b7b7652e6273edf347068902cd
Lbreithaupt <47581541+Lbreithaupt@users.noreply.github.com>
2019-02-12 17:16:12 -0800

    Add files via upload
    No exception handling, very simple program to calculate a two period exponential smoothing value for class.

---
+++ ExponentialSmoothing.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SCMProblemSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+
+            Console.WriteLine("Welcome to Exponential Smoothing");
+            Console.WriteLine();
+          
+          
+          
+                Console.WriteLine("Enter the previous period forecasted value");
+                double forecast;
+                
+                forecast = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter the actual demand value for the period");
+                double demand;
+                demand = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter the alpha value");
+                double alpha;
+                alpha = Convert.ToDouble(Console.ReadLine());
+                double resultCase1;
+                resultCase1 = forecast+(alpha * (demand-forecast));
+                Console.WriteLine("The exponential smoothed value is {0}", resultCase1);
+             
+            
+            Console.WriteLine("Press enter to continue...");
+            Console.Read();
+        }
+          
+    }
+}
